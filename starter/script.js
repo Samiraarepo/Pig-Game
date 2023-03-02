401eb4de8fd3f3ad8a9119c19509a2ef016584a0
SamiraMiss <samirarostami71@gmail.com>
2023-03-02 22:30:12 +0330

    Edited some type error

--- starter/script.js
+++ starter/script.js
@@ -1,10 +1,10 @@
 'use strict';
 //Select Elements
 const score0El = document.querySelector('#score--0');
-const score1El = document.getElementById('socre--1');
-const dice = document.querySelector('.dice');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
 
 //Start conditions
 score0El.textContent = 0;
-score0El.textContent = 0;
-dice.classList.add('hidden');
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
