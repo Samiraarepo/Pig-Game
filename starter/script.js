a33419291dc09328088c770a1091791198b81f64
SamiraMiss <samirarostami71@gmail.com>
2023-03-02 22:29:03 +0330

    get score 1 & 2 elements and add hidden class to dice

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,10 @@
 'use strict';
+//Select Elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('socre--1');
+const dice = document.querySelector('.dice');
+
+//Start conditions
+score0El.textContent = 0;
+score0El.textContent = 0;
+dice.classList.add('hidden');
